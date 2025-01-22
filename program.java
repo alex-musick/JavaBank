2871ea8058c2861d71a283e9447ce456cf5518ea
justinsullivan2004 <sul22007@byui.edu>
2025-01-22 11:48:41 -0700

    changes

--- program.java
+++ program.java
@@ -37,7 +37,7 @@ public class program
                     System.out.println(accounts);
                     System.out.println("Routing Number: ");
                     int selected_routing_number = scan.nextInt();
-                    System.out.println("Account:" + accounts.get(selected_routing_number));
+                    System.out.println("Account: " + accounts.get(selected_routing_number));
 
                     break;
 
